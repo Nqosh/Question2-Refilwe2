3a0588d521a0137aee3fa9016e8547f165c4a8ea
Nqobile <nqobilemoyo@capitecbank.co.za>
2021-09-28 12:05:13 +0200

    First Commit

---
+++ QUESTION 2-Square shape/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUESTION_2_Square_shape
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of your Square");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Square square1 = new Square();
+            square1.Side = length;
+            Console.WriteLine("Perimeter of your Square = " + square1.Side * 4 + " " + "cm");
+            Square square2 = new Square(length);
+            Console.WriteLine("Area of your Square =" + square2.Side * square2.Side + " " + "cm");
+            Console.ReadKey();
+        }
+    }
+
+    public class Square
+    {
+        private double side;
+        public double Side
+        {
+            get { return side; }
+            set { side = value; }
+        }
+
+        public Square()
+        {
+        }
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+    }
+}
